e1745285a6d2cd3817c3bd2726b7cc4ab4cfa460
nicowesse <nico.wesse@gmail.com>
2022-01-18 17:25:51 +0100

    Both axis on StackView

--- src/layout/StackView.cpp
+++ src/layout/StackView.cpp
@@ -8,29 +8,32 @@ void StackView::Wrapper::_drawChildren()
   {
     for (auto & child : children())
     {
-      if (parent->isInsideRectangle(child->getGlobalRect(), true)) child->draw();
+//      if (parent->isInsideRectangle(child->getGlobalRect(), true))
+      if (child->getGlobalRect().getTop() <= parent->getGlobalRect().getBottom() && child->getGlobalRect().getBottom() >= parent->getGlobalRect().getTop()) child->draw();
     }
   }
 }
 
-void StackView::addView(shared_ptr<View> view, unsigned int index)
+void StackView::addViewAt(shared_ptr<View> view, unsigned int index, bool adjustLayout)
 {
   index = ofClamp(index, 0, _subviews.size());
-//  component->setStyle(this->style);
+  view->setStyle(this->style);
   _subviews.insert(_subviews.begin() + index, view);
   wrapper->addChild(view);
   
-  _adjustLayout();
+  if (adjustLayout) _adjustLayout();
 }
 
-void StackView::addView(shared_ptr<View> view)
+void StackView::addView(shared_ptr<View> view, bool adjustLayout)
 {
-  addView(view, _subviews.size());
+  addViewAt(view, _subviews.size(), adjustLayout);
 }
 
-void StackView::addViews(vector<shared_ptr<View>> views)
+void StackView::addViews(vector<shared_ptr<View>> views, bool adjustLayout)
 {
-  for (auto & view : views) addView(view);
+  for (auto & view : views) addView(view, false);
+  
+  if (adjustLayout) _adjustLayout();
 }
 
 void StackView::removeView(shared_ptr<View> view)
@@ -49,7 +52,7 @@ void StackView::_init()
 {
   background = ofxCortex::ui::Background::create();
   auto backgroundStyle = ofxCortex::ui::Style::copy(this->style);
-  backgroundStyle->boxRadius += this->style->margin.top;
+  backgroundStyle->boxRadius += this->style->padding.top;
   background->setStyle(backgroundStyle);
   background->setName(getName() + "::Background");
   background->disableEvents();
@@ -62,50 +65,226 @@ void StackView::_init()
 
 void StackView::_draw()
 {
-  background->drawBackground(style->containerColor);
+  background->drawBackground(style->containerColor, ofFloatColor(1.0f, 0.1f));
   wrapper->draw();
+}
+
+void StackView::_drawMask()
+{
+  background->drawBackground(ofFloatColor::white);
+}
+
+void StackView::_debug()
+{
+  _drawMask();
+  
+  View::_debug();
 }
 
 void StackView::_adjustLayout()
 {
-  glm::vec2 pos = style->margin;
-  float innerWidth = getWidth() - style->margin.left - style->margin.right;
+  View::_recalculateRenderRect();
   
-  for (auto & component : _subviews)
+  if (_axis == VERTICAL)
   {
-    component->setWidth(innerWidth)->setPosition(pos);
-    
-    pos.y += component->getHeight() + style->margin.bottom;
+    if (_getViewsHeight() > this->getHeight() || _distribution == STACK || _distribution == STACK_CENTER || _distribution == STACK_END) _layoutVerticalStack();
+    else if (_distribution == FILL_EQUALLY) _layoutVerticalFillEqually();
+    else if (_distribution == SPACE_EQUALLY) _layoutVerticalEqualSpacing();
+  }
+  else if (_axis == HORIZONTAL)
+  {
+    if (_getViewsWidth() > this->getWidth() || _distribution == STACK || _distribution == STACK_CENTER || _distribution == STACK_END) _layoutHorizontalStack();
+    else if (_distribution == FILL_EQUALLY) _layoutHorizontalFillEqually();
+    else if (_distribution == SPACE_EQUALLY) _layoutHorizontalEqualSpacing();
   }
   
   background->setRect(this->getRect());
-  wrapper->setSize({ this->getWidth(), _getComponentsHeight() });
+  
+  DisplayObject::_adjustLayout();
+}
+
+void StackView::_layoutVerticalStack()
+{
+  glm::vec2 pos = getRenderRect().getPosition();
+  
+  float offsetY = 0.0f;
+  if (_getViewsHeight() < _getInnerHeight())
+  {
+    if (_distribution == STACK_CENTER) offsetY = (_getInnerHeight() - _getViewsHeight()) * 0.5f;
+    else if (_distribution == STACK_END) offsetY = _getInnerHeight() - _getViewsHeight(false);
+  }
+  
+  pos.y += offsetY;
+  
+  for (auto & view : _subviews)
+  {
+    _alignVertical(view, pos);
+    
+    pos.y += view->getHeight() + view->getStyle()->margin.bottom;
+  }
+  
+  wrapper->setSize({ this->getWidth(), _getViewsHeight() });
+}
+
+void StackView::_layoutHorizontalStack()
+{
+  glm::vec2 pos = getRenderRect().getPosition();
+  
+  float offsetX = 0.0f;
+  if (_getViewsWidth() < _getInnerWidth())
+  {
+    if (_distribution == STACK_CENTER) offsetX = (_getInnerWidth() - _getViewsWidth()) * 0.5f;
+    else if (_distribution == STACK_END) offsetX = _getInnerWidth() - _getViewsWidth(false);
+  }
+  
+  pos.x += offsetX;
+  
+  for (auto & view : _subviews)
+  {
+    _alignHorizontal(view, pos);
+    
+    pos.x += view->getWidth() + view->getStyle()->margin.right;
+  }
+  
+  wrapper->setSize({ _getViewsWidth(), this->getHeight() });
+}
+
+void StackView::_layoutVerticalFillEqually()
+{
+  glm::vec2 pos = style->padding;
+  float viewHeight = _getInnerHeight() / _subviews.size();
+  
+  for (auto & view : _subviews)
+  {
+    view->setHeight(viewHeight);
+    _alignVertical(view, pos);
+    
+    pos.y += view->getHeight();
+  }
+  
+  wrapper->setSize({ this->getWidth(), pos.y });
+}
+
+void StackView::_layoutHorizontalFillEqually()
+{
+  glm::vec2 pos = style->padding;
+  float viewWidth = _getInnerWidth() / _subviews.size();
+  
+  for (auto & view : _subviews)
+  {
+    view->setWidth(viewWidth);
+    _alignHorizontal(view, pos);
+    
+    pos.x += view->getWidth() + view->getStyle()->margin.right;
+  }
+  
+  wrapper->setSize({ this->getWidth(), pos.y });
+}
+
+void StackView::_layoutVerticalEqualSpacing()
+{
+  glm::vec2 pos = style->padding;
+  float innerWidth = _getInnerWidth();
+  float innerHeight = _getInnerHeight();
+  
+  float spacing = (innerHeight - _getViewsHeight(false)) / (_subviews.size() - 1);
+  
+  for (auto & view : _subviews)
+  {
+    _alignVertical(view, pos);
+    
+    pos.y += view->getHeight() + spacing;
+  }
+  
+  float height = pos.y - (_subviews.back()->getHeight() + spacing);
+  
+  wrapper->setSize({ this->getWidth(), height });
+}
+
+void StackView::_layoutHorizontalEqualSpacing()
+{
+  glm::vec2 pos = style->padding;
+  float innerHeight = _getInnerHeight();
+  
+  float spacing = (_getInnerWidth() - _getViewsWidth(false)) / (_subviews.size() - 1);
+  
+  for (auto & view : _subviews)
+  {
+    _alignHorizontal(view, pos);
+    
+    pos.x += view->getWidth() + spacing;
+  }
+  
+  float width = pos.x - (_subviews.back()->getWidth() + spacing);
+  
+  wrapper->setSize({ width, this->getHeight() });
 }
 
 void StackView::_mouseScrolled(const ofMouseEventArgs & e)
 {
   View::_mouseScrolled(e);
   
-  if (!(wrapper->getHeight() > this->getHeight())) return;
-  float diff = wrapper->getHeight() - this->getHeight();
+  if (wrapper->getHeight() <= this->getHeight() && wrapper->getWidth() <= this->getWidth()) return;
+  
+  // Scroll Y
+  float diffY = wrapper->getHeight() - this->getHeight();
   float potentialY = wrapper->getY() + e.scrollY;
   
-//  cout << "Mouse scrolled: " << e.scrollY << " Diff: " << diff << " Current Y: " <<  wrapper->getY() << " Potential Y: " << potentialY << endl;
+  if (potentialY > -diffY && potentialY <= 0.0f) wrapper->translate({ 0.0f, e.scrollY });
   
-  if (potentialY > -diff && potentialY <= 0.0f) wrapper->translate({ 0.0f, e.scrollY });
+  // Scroll X
+  float diffX = wrapper->getWidth() - this->getWidth();
+  float potentialX = wrapper->getX() + e.scrollX;
+  
+  if (potentialX > -diffX && potentialX <= 0.0f) wrapper->translate({ e.scrollX, 0.0f });
 }
 
-float StackView::_getComponentsHeight()
+float StackView::_getViewsHeight(bool includeSpacing)
 {
-  float h = style->margin.top;
+  float h = style->padding.top * includeSpacing;
   
-  if (_subviews.size() > 0)
-  {
-    h += _subviews.back()->getBottom() + style->margin.bottom;
-  }
+  for (const auto & view : _subviews) h += view->getHeight() + (style->margin.bottom * includeSpacing);
+  h += style->padding.bottom * includeSpacing;
   
   return h;
 }
 
+float StackView::_getViewsWidth(bool includeSpacing)
+{
+  float w = style->padding.left * includeSpacing;
+  
+  for (const auto & view : _subviews) w += view->getWidth() + (style->margin.right * includeSpacing);
+  w += style->padding.right * includeSpacing;
+  
+  return w;
+}
+
+float StackView::_getInnerWidth()
+{
+  return getRenderRect().width;
+//  return getWidth() - style->padding.left - style->padding.right;
+}
+
+float StackView::_getInnerHeight()
+{
+  return getRenderRect().height;
+//  return getHeight() - style->padding.top - style->padding.bottom;
+}
+
+void StackView::_alignVertical(shared_ptr<View> view, const glm::vec2 & pos)
+{
+  if (_alignment == FILL) { view->setPosition(pos)->setWidth(_getInnerWidth()); }
+  else if (_alignment == ALIGN_START) { view->setPosition(pos); }
+  else if (_alignment == ALIGN_END) { view->setY(pos.y)->alignRight(getRenderRect().getRight()); }
+  else if (_alignment == ALIGN_CENTER) { view->setY(pos.y)->centerX(getRenderRect().getCenter().x); }
+}
+
+void StackView::_alignHorizontal(shared_ptr<View> view, const glm::vec2 & pos)
+{
+  if (_alignment == FILL) { view->setPosition(pos)->setHeight(_getInnerHeight()); }
+  else if (_alignment == ALIGN_START) { view->setPosition(pos); }
+  else if (_alignment == ALIGN_END) { view->setX(pos.x)->alignBottom(getRenderRect().getBottom()); }
+  else if (_alignment == ALIGN_CENTER) { view->setX(pos.x)->centerY(getRenderRect().getCenter().y); }
+}
 
 }}
--- src/layout/StackView.h
+++ src/layout/StackView.h
@@ -12,36 +12,77 @@ class StackView : public ofxCortex::ui::View {
   };
   
 public:
-  StackView(ofRectangle rect = ofRectangle(0, 0, 200, 400)) : View("Layout") {
+  enum Axis {
+    VERTICAL,
+    HORIZONTAL
+  };
+  
+  enum Distribution {
+    STACK,
+    STACK_CENTER,
+    STACK_END,
+    FILL_EQUALLY,
+    SPACE_EQUALLY
+  };
+  
+  enum Alignment {
+    FILL,
+    ALIGN_START,
+    ALIGN_CENTER,
+    ALIGN_END
+  };
+  
+  StackView(ofRectangle rect = ofRectangle(0, 0, 200, 400), Axis axis = VERTICAL, Distribution distribute = STACK, Alignment align = FILL) : View("Layout"), _axis(axis), _distribution(distribute), _alignment(align) {
     this->setRect(rect, false);
     this->disableChildRendering();
   }
   
-  static shared_ptr<StackView> create(ofRectangle rect = ofRectangle(0, 0, 200, 400)) {
-    auto ptr = make_shared<StackView>(rect);
+  static shared_ptr<StackView> create(ofRectangle rect = ofRectangle(0, 0, 200, 400), Axis axis = VERTICAL, Distribution distribute = STACK, Alignment align = FILL) {
+    auto ptr = make_shared<StackView>(rect, axis, distribute, align);
     ptr->_init();
     return ptr;
   }
   
 #pragma mark - Family Releations
-  virtual void addView(shared_ptr<View> view);
-  virtual void addView(shared_ptr<View> view, unsigned int index);
-  virtual void addViews(vector<shared_ptr<View>> views);
+  virtual void addView(shared_ptr<View> view, bool adjustLayout = true);
+  virtual void addViewAt(shared_ptr<View> view, unsigned int index, bool adjustLayout = true);
+  virtual void addViews(vector<shared_ptr<View>> views, bool adjustLayout = true);
   
   virtual void removeView(shared_ptr<View> view);
   virtual void removeAllViews();
   
   
 protected:
+  virtual string _getModule() const override { return "StackView"; };
+  
   void _init();
   virtual void _draw() override;
   virtual void _adjustLayout() override;
+  virtual void _debug() override;
+  
+  virtual void _drawMask() override;
+  
+  void _layoutVerticalStack();
+  void _layoutHorizontalStack();
+  void _layoutVerticalFillEqually();
+  void _layoutHorizontalFillEqually();
+  void _layoutVerticalEqualSpacing();
+  void _layoutHorizontalEqualSpacing();
   
   virtual void _mouseScrolled(const ofMouseEventArgs & e) override;
   
-  float _getComponentsHeight();
+  float _getViewsHeight(bool includeSpacing = true);
+  float _getViewsWidth(bool includeSpacing = true);
+  float _getInnerWidth();
+  float _getInnerHeight();
+  void _alignVertical(shared_ptr<View> view, const glm::vec2 & pos);
+  void _alignHorizontal(shared_ptr<View> view, const glm::vec2 & pos);
   
   vector<shared_ptr<View>> _subviews;
+  
+  Axis _axis;
+  Distribution _distribution;
+  Alignment _alignment;
   
   shared_ptr<Wrapper> wrapper;
   shared_ptr<Background> background;
